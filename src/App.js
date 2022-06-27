995451cd333d05cf922d591cf0c99dc79eba581a
si-shakil <sishakil850@gmail.com>
2022-06-27 15:19:01 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Sidebar */}
+
+      {/* Scrollable Component */}
+
+      {/* Right Side */}
+    </div>
+  );
+}
+
+export default App;
